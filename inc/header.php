bbb38b9eb564e0207a2450a9663674c5cea3395d
RoxanneAsnar <roxanne.asnar@gmail.com>
2017-03-02 18:45:31 +0100

    Add first release of the project

---
+++ inc/header.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-default" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <h1><a class="navbar-brand" href="#">Restaurants Atypiques®</a></h1>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#concept">Le concept</a></li>
+            <li><a href="#createurs">Les créateurs</a></li>
+            <li><a href="#top">Le top 3</a></li>
+            <li><a href="find-us">Où nous trouver</a></li>
+            <li><a href="contact">Contact</a></li>
+        </ul>
+
+
+    </div><!-- /.navbar-collapse -->
+</nav>
---
+++ index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+          crossorigin="anonymous">
+    <link rel="stylesheet"
+          href="css/main.css">
+    <title>Restaurants Atypiques</title>
+</head>
+<body>
+
+    <div class="container">
+        <header>
+            <?php include "inc/header.php"; ?>
+        </header>
+        <main>
+
+            <div id="concept" class="row">
+
+                <h2>Notre concept</h2>
+
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                    <img src="img/wood-interior.jpg" alt="Great restaurant with wood interior" id="wood">
+                </div>
+
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+
+                    <p id="our-concept">
+                        Vous rêviez de manger un couscous suspendu à 30 mètres de hauteur? Vous proposez dans votre steakhouse de déguster un bon steak entourés de fauves en plein air? Nous sommes là pour vous. Notre concept est simple: rassembler tous les restaurants qui proposent un cadre original, un menu (d)étonnant et les mettre en relations avec les clients désireux de nouvelles expériences gastronomiques.
+                    </p>
+
+                </div>
+
+            </div>
+
+            <div id="creators">
+
+            </div>
+
+            <div id="top">
+
+            </div>
+
+            <div id="find-us">
+
+                <div id="list">
+
+                </div>
+
+                <div id="map">
+
+                </div>
+
+            </div>
+
+            <div id="contact">
+
+                <div id="form">
+
+                </div>
+
+            </div>
+
+        </main>
+
+        <footer>
+            <?php include "inc/footer.php"; ?>
+        </footer>
+
+
+        <script src="https://code.jquery.com/jquery-3.1.1.js"
+                integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="
+                crossorigin="anonymous">
+        </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+                integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+                crossorigin="anonymous">
+        </script>
+    </div>
+</body>
+</html>
