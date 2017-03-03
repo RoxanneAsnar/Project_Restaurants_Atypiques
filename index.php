092ff48fb2ce1d84587f7ab83de4a04afcb8e707
LaurentNougaret <uriel3@hotmail.fr>
2017-03-03 13:55:43 +0100

    Add creators part

--- index.php
+++ index.php
@@ -40,7 +40,21 @@
             </div>
 
             <div id="creators">
-
+                <div class="row " ">
+                    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                        <h2>Damien DUPONT</h2>
+                        <p>Un jour j’ai erré dans les rues, et j’ai passé une heure à regarder les devantures des restaurants, essayant de m’imaginer lequel pourrait satisfaire mes papilles. Laissant mon esprit vagabonder, j’ai voyagé, et blabla...</p></br>
+                    </div>
+                    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                        <h2>Julien DUPOND</h2>
+                    <p>Je me rappelle encore du jour où, étant jeune, mes parents m’ont emmené en voyage en Australie chez les aborigènes. J’y ai goûté des yeux d’autruches, un plat que nous autres Européens trouverions absolument abominable. Et j’ai adoré! Depuis ce jour, je me suis toujours dit que j’amènerais la cuisine originale auprès du grand public.</p></br>
+                    </div>
+                    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <h2>Laurent DUPON</h2>
+                    <p>Issu d’une grande école de commerce, Laurent n’en est pas à son coup d’essai. “Restaurants Atypiques” n’est que le dernier-né d’une série de projets originaux: d’une start-up proposant des activités de plein air à une société qui organise des soirées à thèmes
+                    complètement décalées, son maître-mot est “Have fun!”</p></br>
+                    </div>
+                </div>
             </div>
 
             <div id="top">
@@ -78,7 +92,7 @@
             <div id="find-us">
 
                 <div id="list">
-
+                    <?php include "inc/list.php"; ?>
                 </div>
 
                 <div id="map">
