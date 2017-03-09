270727e8c874902b6afb7670c3f393f953718bfc
LaurentNougaret <uriel3@hotmail.fr>
2017-03-09 13:25:11 +0100

    modifying contact form

--- inc/contact.php
+++ inc/contact.php
@@ -2,10 +2,10 @@
     <div id="contact">
         <h2 classe="title_contact">Comment nous joindre?</h2>
         <div class="row" id="large-buttons">
-            <div class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-lg-offset-2 col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <div class=" button1 col-xs-12 col-sm-12 col-md-6 col-lg-6">
                 <button type="button" class="btn btn-primary btn-lg btn-block" id="info-button">Besoin d'informations ?</button>
             </div>
-            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" >
+            <div class="button2 col-xs-12 col-sm-12 col-md-6 col-lg-6" >
                <button type="button" class="btn btn-primary btn-lg btn-block" id="joinus-button">Envie de rejoindre la franchise ?</button>
             </div>
             <div class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-lg-offset-2"></div>
@@ -17,7 +17,7 @@
                     <input type="text" class="form-control" name="name" id="" placeholder="Nom" required> <br/>
                     <input type="text" class="form-control" name="firstname" id="" placeholder="Prénom" required> <br/>
                     <input type="text" class="form-control" name="email" id="" placeholder="mail@email.fr" required><br/>
-                    <textarea name="textarea" class="text-area form-control col-xs-12 col-sm-12 col-md-12 col-lg-12" name="message" id="" type="text" required></textarea>
+                    <textarea name="textarea" class="form-control col-xs-12 col-sm-12 col-md-12 col-lg-12" name="message" id="text-area1" type="text" required></textarea>
                     <button type="submit" class="btn btn-primary send">Envoyer</button>
                 </div>
             </form>
@@ -30,7 +30,7 @@
                     <input type="text" class="form-control" name="" id="firstname" placeholder="Prénom" required> <br/>
                     <input type="text" class="form-control" name="" id="name-restaurant" placeholder="Le nom de votre super restaurant" required> <br/>
                     <input type="text" class="form-control" name="" id="email" placeholder="mail@email.fr"required > <br/>
-                    <textarea name="textarea" class="text-area form-control col-xs-12 col-sm-12 col-md-12 col-lg-12" name="message" id="" type="text" placeholder="Votre message" required>
+                    <textarea name="textarea" class="form-control col-xs-12 col-sm-12 col-md-12 col-lg-12" name="message" id="text-area2" type="text" placeholder="Votre message" required>
                     </textarea><br/>
                     <button type="submit" class="btn btn-primary send">Envoyer</button>
                 </div>
