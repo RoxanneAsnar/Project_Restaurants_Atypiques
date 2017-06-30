019d057cf1fcb41901bfc163c3dccf16b5cfa004
RoxanneAsnar <roxanne.asnar@gmail.com>
2017-06-30 07:58:25 +0200

    fixed css

--- inc/list.php
+++ inc/list.php
@@ -3,22 +3,22 @@
 
         <div class="col-xs-12 col-lg-6" data-toggle="modal" data-target="#table"><h4>LYON</h4>
             <a href="#table_modal" data-toggle="modal" data-target="#table_modal">
-                <img src="/img/la_table_en_braille.jpg" alt="La table en braille"></a>
+                <img src="http://legeekcestchic.eu/wp-content/uploads/2015/03/15-bars-et-restaurants-insolites-venus-tout-droit-du-japon-38.jpg" alt="La table en braille" class="thumbnail"></a>
             <div class="modal fade" id="table_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
-                        <img src="./img/la_table_en_braille.jpg" alt="La table en braille">
+                        <img src="http://legeekcestchic.eu/wp-content/uploads/2015/03/15-bars-et-restaurants-insolites-venus-tout-droit-du-japon-38.jpg" alt="La table en braille" class="big">
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-xs-12 col-lg-6" data-toggle="modal" data-target="#pirates"><h4>SAINT MALO</h4>
             <a href="#pirates_modal" data-toggle="modal" data-target="#pirates_modal">
-                <img src="/img/baie_pirates.jpg" alt="La baie des pirates"></a>
+                <img src="http://www.jungleretreatbali.com/wp-content/uploads/photo-gallery/IMG_0934.JPG" alt="La baie des pirates" class="thumbnail"></a>
             <div class="modal fade" id="pirates_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
-                        <img src="/img/baie_pirates.jpg" alt="La baie des pirates">
+                        <img src="http://www.jungleretreatbali.com/wp-content/uploads/photo-gallery/IMG_0934.JPG" alt="La baie des pirates" class="big">
                     </div>
                 </div>
             </div>
@@ -27,22 +27,22 @@
     <div class="row">
         <div class="col-xs-12 col-lg-6" data-toggle="modal" data-target="#koeben"><h4>BORDEAUX</h4>
             <a href="#koeben_modal" data-toggle="modal" data-target="#koeben_modal">
-                <img src="/img/koeben.jpg" alt="Koeben"></a>
+                <img src="http://www.idealseminaire.fr/images/hotels/21198/9.jpg?1470312261" alt="Koeben" class="thumbnail"></a>
             <div class="modal fade" id="koeben_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
-                        <img src="/img/koeben.jpg" alt="Koeben">
+                        <img src="http://www.idealseminaire.fr/images/hotels/21198/9.jpg?1470312261" alt="Koeben" class="big">
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-xs-12 col-lg-6" data-toggle="modal" data-target="#diner"><h4>TOULOUSE</h4>
             <a href="#diner_modal" data-toggle="modal" data-target="#diner_modal">
-                <img src="/img/marshalls_diner.jpg" alt="Marshall's Diner"></a>
+                <img src="http://www.topoftherock.com/d/topoftherock/media/Dining/Osage_1.jpg?1432810049" alt="Marshall's Diner" class="thumbnail"></a>
             <div class="modal fade" id="table_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
-                        <img src="/img/marshalls_diner.jpg" alt="Marshall's Diner">
+                        <img src="http://www.topoftherock.com/d/topoftherock/media/Dining/Osage_1.jpg?1432810049" alt="Marshall's Diner" class="big">
                     </div>
                 </div>
             </div>
@@ -51,22 +51,22 @@
     <div class="row">
         <div class="col-xs-12 col-lg-6" data-toggle="modal" data-target="#fin"><h4>PARIS</h4>
             <a href="#fin_modal" data-toggle="modal" data-target="#fin_modal">
-                <img src="./img/fin_du_monde.jpg" alt="Le dernier bar avant la fin du monde"></a>
+                <img src="http://www.marvelbuilding.com/wp-content/uploads/2012/11/restaurant-in-Epic-Hotel-Interior-Design-with-Original-Eclectic-Decor.jpg" class="thumbnail" alt="Le dernier bar avant la fin du monde"></a>
             <div class="modal fade" id="fin_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
-                        <img src="./img/fin_du_monde.jpg" alt="Le dernier bar avant la fin du monde">
+                        <img src="http://www.marvelbuilding.com/wp-content/uploads/2012/11/restaurant-in-Epic-Hotel-Interior-Design-with-Original-Eclectic-Decor.jpg" alt="Le dernier bar avant la fin du monde" class="big">
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-xs-12 col-lg-6" data-toggle="modal" data-target="#epicerie"><h4>ORLEANS</h4>
             <a href="#epicerie_modal" data-toggle="modal" data-target="#epicerie_modal">
-                <img src="/img/epicerie_orleanaise.jpg" alt="L'Epicerie orléanaise"></a>
-            <div class="modal fade" id="epicerie_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <img src="https://www.designferia.com/sites/default/files/styles/article_images__s640_/public/field/image/restaurant-original-design-sousmarin-subsix.jpg?itok=J0yEkn-j" alt="L'Epicerie orléanaise" class="thumbnail"></a>
+            <div class="modal fade" id="epicerie_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" >
                 <div class="modal-dialog">
                     <div class="modal-content">
-                        <img src="./img/epicerie_orleanaise.jpg" alt="L'Epicerie orléanaise">
+                        <img src="https://www.designferia.com/sites/default/files/styles/article_images__s640_/public/field/image/restaurant-original-design-sousmarin-subsix.jpg?itok=J0yEkn-j" alt="L'Epicerie orléanaise" class="big">
                     </div>
                 </div>
             </div>
